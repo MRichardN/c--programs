69ddb2b8cef36e3a99d18a40030f9800d0e55a2d
MRichardN <mathengerichard29@gmail.com>
2024-09-10 10:31:23 +0300

    addition program

---
+++ addition.c
@@ -0,0 +1,15 @@
+// program to add 2 numbers (Variables/Data types)
+#include<stdio.h> //scanf(), printf()
+int main () {
+    int a , b, sum; // dec and initialization
+    
+    printf("Enter the first number ");
+    scanf("%d",&a);
+    
+    printf("Enter the 2nd number: ");
+    scanf("%d",&b);
+    sum = a + b;
+    printf("The sum of a and b is %d", sum);
+ 
+  return 0;
+}
