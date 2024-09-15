50f757c754b27dbc137456773a6b11c269eefff7
MRichardN <mathengerichard29@gmail.com>
2024-09-15 12:09:28 +0300

    Second Commit

--- addition.c
+++ addition.c
@@ -1,15 +1,16 @@
 // program to add 2 numbers (Variables/Data types)
+//making changes
 #include<stdio.h> //scanf(), printf()
 int main () {
     int a , b, sum; // dec and initialization
-    
+
     printf("Enter the first number ");
     scanf("%d",&a);
-    
+
     printf("Enter the 2nd number: ");
     scanf("%d",&b);
     sum = a + b;
     printf("The sum of a and b is %d", sum);
- 
+
   return 0;
 }
